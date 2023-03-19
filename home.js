8b7c646f61d7976c49ce76b7bf934bfcb85a46a8
Caroline Martins <martins.tabares@gmail.com>
2023-03-19 20:13:30 -0300

    Ex 9 - carregando detalhes do post

--- home.js
+++ home.js
@@ -15,26 +15,40 @@ async function carregarPosts() {
         const div = document.createElement("div");
         const title = document.createElement("h2");
         const body = document.createElement("p");
+        const buttonDetailPost = document.createElement("button");
 
         title.innerText = post.title;
         body.innerText  = post.body;
+        buttonDetailPost.innerText = "Detalhes do Post";
+        buttonDetailPost.setAttribute("onclick",`detalhesDoPost(${post.id})`);
 
         div.appendChild(title);
         div.appendChild(body); 
-
+        div.appendChild(buttonDetailPost);
         postsContainer.appendChild(div);
     })
+    mostrarBotaoCarregarMais(posts);
+
+} 
+
+function mostrarBotaoCarregarMais(posts) {
     if(temMaisPosts(posts)) {
         buttonLoading.style.display = "block";
         postPage++;        
     } else {
         buttonLoading.style.display = "none";
     }
-
-} 
+}
 
 function temMaisPosts(posts) {
     return posts.length === postLimit;
+}
+
+async function detalhesDoPost(postId) {
+    let url = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
+    const response = await fetch(url);
+    const detailsPost = await response.json();
+    console.log(detailsPost);
 }
 
 carregarPosts();
