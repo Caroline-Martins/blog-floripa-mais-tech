907b2e2243e994c8770f9ed88e0ee19c416c1cf4
Caroline Martins <martins.tabares@gmail.com>
2023-03-19 22:13:12 -0300

    Ex 10 - Exibindo comentarios de um post

--- home.js
+++ home.js
@@ -20,11 +20,15 @@ async function carregarPosts() {
         title.innerText = post.title;
         body.innerText  = post.body;
         buttonDetailPost.innerText = "Detalhes do Post";
-        buttonDetailPost.setAttribute("onclick",`detalhesDoPost(${post.id})`);
-
+        let corpo = JSON.stringify(post)
+        buttonDetailPost.setAttribute("data-toggle", "modal")
+        buttonDetailPost.setAttribute("data-target", "#exampleModal")
+        buttonDetailPost.setAttribute("onclick", `detalhesDoPost(${corpo})`);
+        
         div.appendChild(title);
         div.appendChild(body); 
         div.appendChild(buttonDetailPost);
+        
         postsContainer.appendChild(div);
     })
     mostrarBotaoCarregarMais(posts);
@@ -44,11 +48,39 @@ function temMaisPosts(posts) {
     return posts.length === postLimit;
 }
 
-async function detalhesDoPost(postId) {
-    let url = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
+async function detalhesDoPost(post) {
+    let url = `https://jsonplaceholder.typicode.com/posts/${post.id}/comments`;
     const response = await fetch(url);
     const detailsPost = await response.json();
     console.log(detailsPost);
+
+    const postObj = {
+        ...post,
+        'comentarios': JSON.stringify(detailsPost)
+    }
+    mostrarModal(postObj);
+}
+
+function mostrarModal(postObj) {
+    let modal = document.querySelector("#modal-body")
+
+    const div = document.createElement("div");
+    const title = document.createElement("h2");
+    const body = document.createElement("p");
+    const comments = document.createElement("p");
+    
+    title.innerHTML = `<b>Titulo:</b> ${postObj.title}`;
+    body.innerHTML  = `<b>Descrição:</b> ${postObj.body}`;
+    comments.innerHTML = `<b>Comentários:</b> ${postObj.comentarios}`;
+
+        
+    div.appendChild(title);
+    div.appendChild(body); 
+    div.appendChild(comments);
+        
+    modal.appendChild(div);
+
+    //modal.innerText = JSON.stringify(postObj);
 }
 
 carregarPosts();
